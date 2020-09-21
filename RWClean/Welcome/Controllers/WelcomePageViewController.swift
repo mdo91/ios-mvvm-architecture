8055e40150e96aa19ffbb6749f0a6acd93c066b4
mdo91 <hesoka_mxx@yahoo.com>
2020-09-21 14:48:26 +0300

    welcome page set yp

---
+++ RWClean/Welcome/Controllers/WelcomePageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePageViewController.swift
+//  RWClean
+//
+//  Created by Mdo on 21/09/2020.
+//  Copyright © 2020 Mdo. All rights reserved.
+//
+
+import UIKit
+
+class WelcomePageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ RWClean/Welcome/Controllers/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  RWClean
+//
+//  Created by Mdo on 21/09/2020.
+//  Copyright © 2020 Mdo. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
